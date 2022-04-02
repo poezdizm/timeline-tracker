6330b31433b836a30820bd6e258e7bcd9d6f8ed6
Dmitry Lysenkov <u_062pn>
2022-04-02 20:06:34 +0300

    Initial commit

---
+++ src/test/java/ru/poezdizm/timelinetracker/TimelineTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.poezdizm.timelinetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimelineTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
